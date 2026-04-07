9f687bbc8a42fd09dd79d90cdb9137823fc5cbc4
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-04-07 12:48:16 +0530

    initial commit

---
+++ ReactTaskCRUD/my-react-app/src/AllUsers.jsx
@@ -0,0 +1,160 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const API = "http://localhost:3001/users";
+
+function AllUsers() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const res = await axios.get(API);
+    setUsers(res.data);
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>All Users</h2>
+
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h3>Name : {user.username}</h3>
+            <h3>Password: {user.password}</h3>
+            <h3>Email: {user.gmail}</h3>
+
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+export default AllUsers;
+// import {useNavigate} from "react-router-dom"
+
+// // export default AllUsers;
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+
+// const API = "http://localhost:3001/users";
+
+// function Home() {
+//   const [form, setForm] = useState({
+//     username: "",
+//     password: "",
+//     gmail: "",
+//   });
+
+//   const [users, setUsers] = useState([]);
+
+//   const [editId, setEditId] = useState(null);
+
+//   console.log(form);
+
+//   useEffect(() => {
+//     fetchUsers();
+//   }, []);
+
+//   const fetchUsers = async () => {
+//     const res = await axios.get(API, form);
+//     console.log(res);
+//     setUsers(res.data);
+//     console.log(users);
+//   };
+
+//   const handleChange = (e) => {
+//     console.log(e);
+//     setForm({ ...form, [e.target.name]: e.target.value });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     //At first there will be no editId because we have no user
+//     if (editId) {
+//       await axios.put(`${API}/${editId}`, form);
+//       setEditId(null);
+//     } else {
+//       const res = await axios.post(API, form);
+//     }
+//     setForm({
+//       username: "",
+//       password: "",
+//       gmail: "",
+//     });
+
+//     fetchUsers();
+//   };
+
+//   const handleDelete = async (id) => {
+//     console.log("delete");
+//     await axios.delete(`${API}/${id}`);
+//     fetchUsers();
+//   };
+
+//   const handleEdit = async (user, editId) => {
+//     console.log("edit");
+//     setForm(user);
+
+//     setEditId(user.id);
+//   };
+
+//   return (
+//     <div>
+//       {/* <form onSubmit={handleSubmit}>
+//         <h3>Name</h3>
+//         <input
+//           type="text"
+//           name="username"
+//           placeholder="Enter Name"
+//           value={form.username}
+//           onChange={handleChange}
+//         />
+//         <h3>Gmail</h3>
+//         <input
+//           type="email"
+//           name="gmail"
+//           placeholder="Enter Email"
+//           value={form.gmail}
+//           onChange={handleChange}
+//         />
+//         <h3>Password</h3>
+//         <input
+//           type="text"
+//           name="password"
+//           placeholder="Enter Password"
+//           value={form.password}
+//           onChange={handleChange}
+//         />
+//         <button type="submit">Add User</button>
+//       </form> */}
+
+//       <h1>All Users</h1>
+//       <hr />
+//       {users.map((user) => {
+//         return (
+//           <div key={user.id}>
+//             <h3>Name : {user.username}</h3>
+//             <h3>Password: {user.password}</h3>
+//             <h3>Email: {user.gmail}</h3>
+//             <button onClick={() => handleEdit(user)}>edit</button>
+//             <button
+//               onClick={() => {
+//                 handleDelete(user.id);
+//               }}
+//             >
+//               delete
+//             </button>
+//             <hr />
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default Home;
--- ReactTaskCRUD/my-react-app/src/App.jsx
+++ ReactTaskCRUD/my-react-app/src/App.jsx
@@ -1,109 +1,20 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-const API = "http://localhost:3001/users";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import AllUsers from "./AllUsers";
+import Home from "./Home";
 
 function App() {
-  const [form, setForm] = useState({
-    username: "",
-    password: "",
-    gmail: "",
-  });
-
-  const [users, setUsers] = useState([]);
-  const [editId, setEditId] = useState(null);
-
-  // 🔹 Fetch Users (READ)
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const fetchUsers = async () => {
-    const res = await axios.get(API);
-    setUsers(res.data);
-  };
-
-  // 🔹 Handle Input
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
-  // 🔹 Create or Update User
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (editId) {
-      // UPDATE
-      await axios.put(`${API}/${editId}`, form);
-      setEditId(null);
-    } else {
-      // CREATE
-      await axios.post(API, form);
-    }
-
-    setForm({ username: "", password: "", gmail: "" });
-    fetchUsers();
-  };
-
-  // 🔹 Delete User
-  const handleDelete = async (id) => {
-    await axios.delete(`${API}/${id}`);
-    fetchUsers();
-  };
-
-  // 🔹 Edit User
-  const handleEdit = (user) => {
-    setForm(user);
-    setEditId(user.id);
-  };
-
   return (
-    <div style={{ padding: "20px" }}>
-      <h2>CRUD App</h2>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/users">All Users</Link>
+      </nav>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={form.username}
-          onChange={handleChange}
-        />
-
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={form.password}
-          onChange={handleChange}
-        />
-
-        <input
-          type="email"
-          name="gmail"
-          placeholder="Gmail"
-          value={form.gmail}
-          onChange={handleChange}
-        />
-
-        <button type="submit">{editId ? "Update User" : "Add User"}</button>
-      </form>
-
-      <hr />
-
-      <h3>Users List</h3>
-
-      {users.map((user) => (
-        <div key={user.id}>
-          <p>
-            {user.username} | {user.gmail}
-          </p>
-
-          <button onClick={() => handleEdit(user)}>Edit</button>
-          <button onClick={() => handleDelete(user.id)}>Delete</button>
-        </div>
-      ))}
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<AllUsers />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
