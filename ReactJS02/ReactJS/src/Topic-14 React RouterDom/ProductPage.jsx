fc68434a4a2ca87f3ce3d05d71a80f825d514a22
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-05-08 14:58:36 +0530

    commit

--- ReactJS02/ReactJS/src/Topic-14 React RouterDom/ProductPage.jsx
+++ ReactJS02/ReactJS/src/Topic-14 React RouterDom/ProductPage.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+//?304 === data is coming from the browser-cache not from the backend/sever.
 
 const ProductPage = () => {
+  const [singleProduct, setSingleProduct] = useState({});
+  //~useParams hook is basically used for getting the :id from the url.
+  const { id } = useParams();
+  useEffect(() => {}, []);
+  console.log(params);
+
   return (
     <div className="center-container">
       <div className="left"></div>
